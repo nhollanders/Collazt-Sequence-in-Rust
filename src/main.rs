c61c909f11a764489147f15aade8b297a765ca0b
NonT <NonT5498@gmail.com>
2025-07-25 01:50:20 -0700

    prints the sequence now

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,12 @@ fn main() {
 
     // now we can generate the collatz sequence
 
-    let mut collatz_sez: linked_list::LinkedList<i64> = linked_list::LinkedList::new();
+    let mut collatz_seq: linked_list::LinkedList<i64> = linked_list::LinkedList::new();
     let mut highest_number = 0;
     let mut current_num = input_num;
     let mut iterations = 0;
 
-    collatz_sez.push_back(current_num);
+    collatz_seq.push_back(current_num);
 
     while current_num != 1 {
         if is_even(current_num) {
@@ -37,7 +37,7 @@ fn main() {
             current_num = (3 * current_num) + 1;
         }
 
-        collatz_sez.push_back(current_num);
+        collatz_seq.push_back(current_num);
 
         if current_num > highest_number {
             highest_number = current_num;
@@ -50,13 +50,26 @@ fn main() {
         }
     }
 
-    println!("Finished Calculating Collatz Sequence with a total of {} numbers.\nHighest Number: {}", collatz_sez.len()-1, highest_number); // minus one to account for the starting number that gets added
+    println!("Finished Calculating Collatz Sequence with a total of {} numbers.\nHighest Number: {}", collatz_seq.len()-1, highest_number); // minus one to account for the starting number that gets added
     
+    print_collatz_link_list(collatz_seq);
     
     println!("Press Enter to close...");
 
     let mut wait = String::new();
     let _ = stdin().read_line(&mut wait);
+}
+
+fn print_collatz_link_list(list: linked_list::LinkedList<i64>) {
+    let mut print_string = String::new();
+
+    print_string = print_string + "Sequence:";
+
+    for seq_num in list {
+        print_string = print_string + " -> " + &seq_num.to_string();
+    }
+
+    println!("{}", print_string);
 }
 
 fn is_even(number: i64) -> bool {
